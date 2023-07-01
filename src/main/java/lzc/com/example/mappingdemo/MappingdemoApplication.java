c51a4669c0f1966ae29ae9c411578e5772bec674
ZL&EL <lzc123456ab@gmail.com>
2023-06-30 21:42:15 -0400

    01

---
+++ src/main/java/lzc/com/example/mappingdemo/MappingdemoApplication.java
@@ -0,0 +1,21 @@
+package lzc.com.example.mappingdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MappingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+}
